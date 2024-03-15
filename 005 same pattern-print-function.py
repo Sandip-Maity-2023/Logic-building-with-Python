bc9b9abc2066cddfc7e3a9678039de19e75dae02
Sandip-Maity-2023 <12sandip125@gmail.com>
2024-03-15 22:41:02 +0530

    pycharm

--- 005 same pattern-print-function.py
+++ 005 same pattern-print-function.py
@@ -1,6 +1,5 @@
 
-def pyramid(rows):
-    for i in range(rows):
-         print(' '*(rows-i-1)+'*'*(2*i+1))
-    print()
+def pylance (7):
+    for i in range(7):
+         print(' '*(7-i-1)+'*'*(2*i+1))
 
