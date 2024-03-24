0dd28c7585bcc6907356ef47db8faa7c614666af
Sandip-Maity-2023 <12sandip125@gmail.com>
2024-03-24 13:21:20 +0530

    Pycharm

---
+++ 008 simple calculator.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+print("Sum:",a+b)
+print("Difference:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+print("Modulus:",a%b)
+print("Exponential/Exponential:",a**b)
+print("Floor Division:",a//b)   #Floor division in Python is an operation that divides two numbers and rounds the result down to the nearest integer.
