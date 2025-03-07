3d393619fd148c5b7cc9b80e56448865cd76298e
Sandip-Maity-2023 <12sandip125@gmail.com>
2025-03-07 20:25:59 +0530

    update

--- Exploratory Data Analysis Lab/01 pandas.py
+++ Exploratory Data Analysis Lab/01 pandas.py
@@ -1,3 +1,11 @@
 import pandas as pd
 s=pd.Series([1,2,3,4,5])
-s
+a={'Name':['Alice','Bob'],'Age':[25,26]}
+df=pd.DataFrame(a)
+print(df)
+
+
+
+import numpy as np
+b=np.array([1,2,3,4,5])
+print(b)
