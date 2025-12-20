2b7ad98113ccfd5bb48ea6ee527fdb10571129ba
Sandip-Maity-2023 <12sandip125@gmail.com>
2025-12-20 08:09:13 +0530

    update

--- python for college/01 list odd & even.py
+++ python for college/01 list odd & even.py
@@ -1,10 +1,12 @@
 def is_odd_even():
     print('Even nums till input')
+
     for i in range(0,101):
         if i%2==0:
             print(i,end=' ')
 
     print('\nOdd nums till input')
+    
     for i in range(0,101):
         if i%2 !=0:
             print(i,end=' ')
